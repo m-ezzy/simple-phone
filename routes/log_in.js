d40fa05d349d4f6c62c8c06b3e75ea29908f75a3
murtaza ezzy <murtazaezzy1001@gmail.com>
2022-11-27 08:11:58 +0530

    v

--- routes/log_in.js
+++ routes/log_in.js
@@ -9,8 +9,9 @@ router.post("/", async (req, res) => {
 
 	let query = `SELECT user_name,pass_word FROM users WHERE user_name='${user_name}' AND pass_word='${pass_word}'`;
 	let rows = await db.query(query).catch(err => { console.log(err); });
+	console.log(rows);
 
-	if (rows.length) {
+	if (rows && rows.length) {
 		res.cookie("user_name", user_name, {maxAge: '3600000'}); //1 hour
 		//res.contentType('text/html');
 		res.redirect("/");
@@ -19,7 +20,7 @@ router.post("/", async (req, res) => {
 		let rows2 = await db.query(query2).catch(err => { console.log(err); });
 
 		res.contentType('text/json');
-		if (rows2.length) {
+		if (rows2 && rows2.length) {
 			res.send(`pass word ${pass_word} is incorrect !`);
 		} else {
 			res.send(`user name ${user_name} is incorrect !`);
